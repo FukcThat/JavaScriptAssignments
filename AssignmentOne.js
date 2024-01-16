91ba40f07e1567d89286a169c7dee3083106e747
FukcThat <63652e636f6465@gmail.com>
2024-01-16 18:42:36 +0100

    Finished First JavaScript Assignment

---
+++ AssignmentOne.js
@@ -0,0 +1,23 @@
+// 1. //
+
+console.log(23+97)
+
+// 2. // 
+
+console.log(1+2+3+4+5+6)
+
+// 3. //
+
+console.log((4+6+9)/77)
+
+// 4. // 
+
+let a = 10
+console.log(a)
+
+// 5. //
+
+let MAX = 57
+let actual = MAX-13
+let percentage = actual/MAX
+
