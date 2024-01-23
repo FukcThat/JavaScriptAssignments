236648d6cb00b99c0648029acd3b8d64538dea39
FukcThat <63652e636f6465@gmail.com>
2024-01-23 22:02:19 +0100

    Solved all Errors in Odin Project Error Lesson

---
+++ Errors.js
@@ -0,0 +1,43 @@
+// 1. // 
+
+let a = "hello";
+let b = "World";
+let c = a+b;
+
+console.log(c);
+
+// 2. // 
+
+
+
+function add() {
+
+    let d = 5;
+    let e = 10;
+    let result = d + e;
+
+    console.log(result);
+}
+
+add();
+
+
+
+// 3. //
+
+function helloWorld() {
+    console.log("Hello World!")
+  }
+
+helloWorld();
+
+// 4. //
+
+const str1 = "Hello";
+const str2 = "World!";
+const message = str1.concat(str2);
+
+console.log(message);
+
+// 5. //
+
